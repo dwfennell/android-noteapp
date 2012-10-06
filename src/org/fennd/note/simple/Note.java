5b8199f62c7e9401c74cc20eb233fdbc10c94ad1
Dustin Fennell <dustin@fennd.org>
2012-10-06 13:10:29 -0700

    Note persistence is up.

--- src/org/fennd/note/simple/Note.java
+++ src/org/fennd/note/simple/Note.java
@@ -1,7 +1,11 @@
 package org.fennd.note.simple;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import android.app.Activity;
 import android.content.Context;
@@ -18,8 +22,8 @@ public class Note extends Activity {
 
 	// TODO: Respond to activity button presses.
 	// TODO: Respond (gracefully?) to title changes.
-	// TODO: Note body io.
 	// TODO: validate note names?
+	// TODO: Improve efficiency/robustness of persistence. 
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -27,33 +31,37 @@ public class Note extends Activity {
 		setContentView(R.layout.activity_note);
 
 		noteState = getPreferences(0);
-		restoreLastNote();
 	}
-
+	
+	
 	@Override
 	protected void onResume() {
 		super.onResume();
 
 		restoreLastNote();
 	}
-
+	
+	
 	@Override
 	protected void onPause() {
 		super.onPause();
 
-		noteState.edit().putString("lastActive", activeNoteName);
+		noteState.edit().putString("lastActive", activeNoteName).commit();
 		saveCurrentNote();
 	}
-
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_note, menu);
 		return true;
 	}
-
+	
+	
 	private void restoreLastNote() {
-
-		if (noteState.getInt("numberOfNotes", 0) == 0) {
+		int numberOfNotes = noteState.getInt("numberOfNotes", 0);
+		
+		if (numberOfNotes == 0) {
 			// No notes have been created yet.
 
 			noteState.edit().putInt("untitledCount", 1).commit();
@@ -73,13 +81,16 @@ public class Note extends Activity {
 		EditText bodyWidget = (EditText) findViewById(R.id.note_body);
 		bodyWidget.setText(activeNoteBody);
 	}
-
+	
+	
 	private void saveCurrentNote() {
-
 		// Deal with changes to the note title.
 		EditText titleWidget = (EditText) findViewById(R.id.note_title);
 		String noteTitle = titleWidget.getText().toString();
-
+		
+		EditText bodyWidget = (EditText) findViewById(R.id.note_body);
+		activeNoteBody = bodyWidget.getText().toString();
+		
 		if (!noteTitle.equals(activeNoteName)) {
 			// Note title has been changed.
 			// TODO handle note title changes here?
@@ -92,28 +103,48 @@ public class Note extends Activity {
 			fos.close();
 
 		} catch (IOException e) {
-			// TODO: Find out what should be done here.
-
+			// TODO: Exception.
 			e.printStackTrace();
 		}
-
 	}
-
+	
+	
 	private void createNewNote(String name) {
 		// Increment note count.
 		int num = noteState.getInt("numberOfNotes", 0);
 		num++;
 		noteState.edit().putInt("numberOfNotes", num).commit();
-
+		// TODO ? 
 	}
-
+	
+	
 	private String fetchNoteBody(String name) {
+		
+		final Set<String> files = new HashSet<String>(Arrays.asList(fileList()));
+		
+		if (!files.contains(name)) {
+			// File does not exist.
+			return "";
+		}
+		
+		try {
+			FileInputStream fis = openFileInput(activeNoteName);
+			byte[] input = new byte[fis.available()];
+			
+			while (fis.read(input) != -1) {
+			}
+			
+			return new String(input);
 
-		// TODO: Load note text from file, if it exists.
-
+		} catch (IOException e) {
+			// TODO exception.
+			e.printStackTrace();
+		}
+		
 		return "";
 	}
-
+	
+	
 	private String getUntitledName(Integer count) {
 		return "Untitled Note " + count.toString();
 	}
