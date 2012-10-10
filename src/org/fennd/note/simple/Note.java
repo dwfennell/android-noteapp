1ffd4111753c4278d03ef16621877da19f33d8f0
Dustin Fennell <dustin@fennd.org>
2012-10-10 08:45:08 -0700

    Progress on note save/restore.

--- src/org/fennd/note/simple/Note.java
+++ src/org/fennd/note/simple/Note.java
@@ -10,8 +10,10 @@ import java.util.Set;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.EditText;
 
 public class Note extends Activity {
@@ -23,7 +25,8 @@ public class Note extends Activity {
 	// TODO: Respond to activity button presses.
 	// TODO: Respond (gracefully?) to title changes.
 	// TODO: validate note names?
-	// TODO: Improve efficiency/robustness of persistence. 
+	// TODO: Improve efficiency/robustness of persistence.
+	// TODO: Note renaming isn't working correctly.
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -32,16 +35,14 @@ public class Note extends Activity {
 
 		noteState = getPreferences(0);
 	}
-	
-	
+
 	@Override
 	protected void onResume() {
 		super.onResume();
 
 		restoreLastNote();
 	}
-	
-	
+
 	@Override
 	protected void onPause() {
 		super.onPause();
@@ -49,18 +50,28 @@ public class Note extends Activity {
 		noteState.edit().putString("lastActive", activeNoteName).commit();
 		saveCurrentNote();
 	}
-	
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_note, menu);
 		return true;
 	}
-	
-	
+
+	public void newButtonClick(View view) {
+		System.out.print("button clicked");
+	}
+
+	public void noteListButtonClick(View view) {
+		System.out.print("button clicked");
+	}
+
+	public void settingsButtonClick(View view) {
+		System.out.print("button clicked");
+	}
+
 	private void restoreLastNote() {
 		int numberOfNotes = noteState.getInt("numberOfNotes", 0);
-		
+
 		if (numberOfNotes == 0) {
 			// No notes have been created yet.
 
@@ -68,7 +79,12 @@ public class Note extends Activity {
 			activeNoteName = getUntitledName(1);
 			activeNoteBody = "";
 
-			createNewNote(activeNoteName);
+			// Increment note count.
+			// TODO: Do we even need to keep a count? Would a boolean do here?
+			Editor stateEditor = noteState.edit();
+			stateEditor.putInt("numberOfNotes",
+					noteState.getInt("numberOfNotes", 0) + 1);
+			stateEditor.commit();
 
 		} else {
 			activeNoteName = noteState.getString("lastActive", "");
@@ -81,19 +97,20 @@ public class Note extends Activity {
 		EditText bodyWidget = (EditText) findViewById(R.id.note_body);
 		bodyWidget.setText(activeNoteBody);
 	}
-	
-	
+
 	private void saveCurrentNote() {
 		// Deal with changes to the note title.
 		EditText titleWidget = (EditText) findViewById(R.id.note_title);
 		String noteTitle = titleWidget.getText().toString();
-		
+
 		EditText bodyWidget = (EditText) findViewById(R.id.note_body);
 		activeNoteBody = bodyWidget.getText().toString();
-		
+
 		if (!noteTitle.equals(activeNoteName)) {
 			// Note title has been changed.
-			// TODO handle note title changes here?
+
+			deleteFile(activeNoteName);
+			activeNoteName = noteTitle;
 		}
 
 		try {
@@ -107,44 +124,31 @@ public class Note extends Activity {
 			e.printStackTrace();
 		}
 	}
-	
-	
-	private void createNewNote(String name) {
-		// Increment note count.
-		int num = noteState.getInt("numberOfNotes", 0);
-		num++;
-		noteState.edit().putInt("numberOfNotes", num).commit();
-		// TODO ? 
-	}
-	
-	
+
 	private String fetchNoteBody(String name) {
-		
 		final Set<String> files = new HashSet<String>(Arrays.asList(fileList()));
-		
+
 		if (!files.contains(name)) {
 			// File does not exist.
 			return "";
 		}
-		
+
 		try {
 			FileInputStream fis = openFileInput(activeNoteName);
 			byte[] input = new byte[fis.available()];
-			
 			while (fis.read(input) != -1) {
 			}
-			
+
 			return new String(input);
 
 		} catch (IOException e) {
 			// TODO exception.
 			e.printStackTrace();
 		}
-		
+
 		return "";
 	}
-	
-	
+
 	private String getUntitledName(Integer count) {
 		return "Untitled Note " + count.toString();
 	}
